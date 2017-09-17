3c693b912ca1ca067cb6574d17c198219ab2cd7b
Andrey Sorokin <anderysorokin@gmail.com>
2017-09-17 19:52:48 +0300

    Release
    all test passed

--- src/index.js
+++ src/index.js
@@ -3,53 +3,37 @@ module.exports = function check(str, bracketsConfig) {
 var tempStack=[];
 var config=bracketsConfig;
 var tempFlag=false;
+var sameBracketsStack=[];
 
-/*for(let i=0;i<testStr.length;i++){
-	for(var k=0;k<config.length;k++){
-		if(testStr[i] === config[k][0]){
-			if(config[k][0]===config[k][1]){
-				if(!tempFlag){
-					tempFlag=true;
-					//Случай, когда скобки однотипные и встретилась она первый раз
-				}else{
-					tempFlag=false;
-				}
-			} else{
-			tempStack.push(config[k][1]);
-		}} else{
-    	if(testStr[i] === config[k][1]){
-		if(!(testStr[i] === tempStack.pop())){
-			return false;
-		}
-      }
-    }
-	}
-}
-
-if(tempStack.length === 0) {return true; }else {return false;}*/
 for(let i=0;i<testStr.length;i++){
     for(let k=0;k<config.length;k++){
-        if(testStr[i] === config[k][1]){
-            if(config[k][1] === config[k][0]){
-                if(tempFlag){
-                   alert("вторая симметричная");
-                   tempFlag=false;
-
-                   if(!(testStr[i] === tempStack.pop())){
-                     //  alert("бида")
-                     return false;
-                   }else{
-					  tempStack.pop(); 
-				   }
-                }else{
-                   alert("1 симметричная");
-                   tempStack.push(config[k][1]);
-                   tempFlag=true;
-                }  
-            } else{
-               tempStack.push(config[k][1]);
-            }
-        }    
+        if(testStr[i] === config[k][0]){
+            if(!(config[k][0] === config[k][1])){
+				tempStack.unshift(config[k][1]);
+			}else{
+				if(!(sameBracketsStack[0] === testStr[i])){
+					tempStack.unshift(config[k][1]);
+					sameBracketsStack.unshift(config[k][1]);
+				}else{
+					if(!(testStr[i] === tempStack[0])){
+					   return false;
+				    }else{
+						tempStack.shift();
+						sameBracketsStack.shift();
+					}
+					
+					
+				}
+			}
+        }else{
+			if(testStr[i] === config[k][1]){
+				if(!(testStr[i] === tempStack[0])){
+					return false;
+				}else{
+					tempStack.shift();
+				}
+			}
+		}    
     }
 }
 if(tempStack.length === 0) {return true; }else {return false;}
